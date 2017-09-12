d7d65b74f31c0b3ddbbe308d4a6211fda2b23d53
Vladimir Makeev <v.makeev88@gmail.com>
2017-09-12 23:01:32 +0400

    Переписал Visitor для парсинга объявлений переменных в адекватный вид

---
+++ SqlFirst.Providers.MsSqlServer/VariableDeclarations/DeclarationVisitor.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SqlFirst.Core.Parsing;
+using SqlFirst.Providers.MsSqlServer.VariableDeclarations.Generated;
+
+namespace SqlFirst.Providers.MsSqlServer.VariableDeclarations
+{
+	internal class DeclarationVisitor : SqlVariableDeclarationsBaseVisitor<QueryParamInfo>
+	{
+		/// <inheritdoc />
+		protected override QueryParamInfo AggregateResult(QueryParamInfo aggregate, QueryParamInfo nextResult)
+		{
+			if (aggregate == null)
+			{
+				return nextResult;
+			}
+
+			if (nextResult == null)
+			{
+				return aggregate;
+			}
+
+			if (aggregate.Length == null && nextResult.Length != null)
+			{
+				aggregate.Length = nextResult.Length;
+			}
+
+			if (aggregate.DbName == null && nextResult.DbName != null)
+			{
+				aggregate.DbName = nextResult.DbName;
+			}
+
+			if (aggregate.DbType == null && nextResult.DbType != null)
+			{
+				aggregate.DbType = nextResult.DbType;
+			}
+
+			if (aggregate.DefaultValue == null && nextResult.DefaultValue != null)
+			{
+				aggregate.DefaultValue = nextResult.DefaultValue;
+			}
+
+			return aggregate;
+		}
+
+		/// <inheritdoc />
+		public override QueryParamInfo VisitVariable(SqlVariableDeclarationsParser.VariableContext context)
+		{
+			string variableName = context.GetRuleContexts<SqlVariableDeclarationsParser.IdentifierContext>().Single().GetText();
+
+			return new QueryParamInfo
+			{
+				DbName = variableName
+			};
+		}
+
+		/// <inheritdoc />
+		public override QueryParamInfo VisitTypeName(SqlVariableDeclarationsParser.TypeNameContext context)
+		{
+			IEnumerable<string> typeNameParts = context.GetRuleContexts<SqlVariableDeclarationsParser.IdentifierContext>().Select(p => p.GetText());
+
+			return new QueryParamInfo
+			{
+				DbType = string.Join(" ", typeNameParts)
+			};
+		}
+
+		/// <inheritdoc />
+		public override QueryParamInfo VisitLength(SqlVariableDeclarationsParser.LengthContext context)
+		{
+			var maxValueContext = new Lazy<SqlVariableDeclarationsParser.MaxValueContext>(() => context.GetRuleContext<SqlVariableDeclarationsParser.MaxValueContext>(0));
+			var intContext = new Lazy<SqlVariableDeclarationsParser.IntValueContext>(() => context.GetRuleContext<SqlVariableDeclarationsParser.IntValueContext>(0));
+
+			QueryParamInfo result = null;
+
+			if (intContext.Value != null)
+			{
+				result = new QueryParamInfo
+				{
+					Length = intContext.Value.GetText()
+				};
+			}
+			else if (maxValueContext.Value != null)
+			{
+				result = new QueryParamInfo
+				{
+					Length = maxValueContext.Value.GetText()
+				};
+			}
+
+			return result;
+		}
+
+		/// <inheritdoc />
+		public override QueryParamInfo VisitValue(SqlVariableDeclarationsParser.ValueContext context)
+		{
+			var stringContext = new Lazy<SqlVariableDeclarationsParser.StringValueContext>(() => context.GetRuleContext<SqlVariableDeclarationsParser.StringValueContext>(0));
+			var intContext = new Lazy<SqlVariableDeclarationsParser.IntValueContext>(() => context.GetRuleContext<SqlVariableDeclarationsParser.IntValueContext>(0));
+			var floatContext = new Lazy<SqlVariableDeclarationsParser.FloatValueContext>(() => context.GetRuleContext<SqlVariableDeclarationsParser.FloatValueContext>(0));
+
+			QueryParamInfo result = null;
+
+			if (stringContext.Value != null)
+			{
+				result = new QueryParamInfo
+				{
+					DefaultValue = stringContext.Value.GetText()?.Trim('\'')
+				};
+			}
+			else if (intContext.Value != null)
+			{
+				result = new QueryParamInfo
+				{
+					DefaultValue = int.Parse(intContext.Value.GetText())
+				};
+			}
+			else if (floatContext.Value != null)
+			{
+				result = new QueryParamInfo
+				{
+					DefaultValue = float.Parse(floatContext.Value.GetText())
+				};
+			}
+
+			return result;
+		}
+	}
+}
--- SqlFirst.Providers.MsSqlServer/VariableDeclarations/QueryParamInfoVisitor.cs
+++ SqlFirst.Providers.MsSqlServer/VariableDeclarations/QueryParamInfoVisitor.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using SqlFirst.Core.Parsing;
 using SqlFirst.Providers.MsSqlServer.VariableDeclarations.Generated;
@@ -7,87 +6,30 @@ using static SqlFirst.Providers.MsSqlServer.VariableDeclarations.Generated.SqlVa
 
 namespace SqlFirst.Providers.MsSqlServer.VariableDeclarations
 {
-	public class QueryParamInfoVisitor : SqlVariableDeclarationsBaseVisitor<IEnumerable<IQueryParamInfo>>
+	internal class QueryParamInfoVisitor : SqlVariableDeclarationsBaseVisitor<IEnumerable<IQueryParamInfo>>
 	{
-		private QueryParamInfo _current;
-		private List<IQueryParamInfo> _results;
-
 		/// <inheritdoc />
-		public override IEnumerable<IQueryParamInfo> VisitRoot(RootContext context)
+		protected override IEnumerable<IQueryParamInfo> AggregateResult(IEnumerable<IQueryParamInfo> aggregate, IEnumerable<IQueryParamInfo> nextResult)
 		{
-			_results = new List<IQueryParamInfo>();
-			base.VisitRoot(context);
-			return _results;
+			if (aggregate == null)
+			{
+				return nextResult;
+			}
+
+			if (nextResult == null)
+			{
+				return aggregate;
+			}
+
+			return aggregate.Concat(nextResult);
 		}
 
 		/// <inheritdoc />
 		public override IEnumerable<IQueryParamInfo> VisitDeclaration(DeclarationContext context)
 		{
-			_current = new QueryParamInfo();
-			VisitChildren(context);
-			_results.Add(_current);
-			return null;
-		}
-
-		/// <inheritdoc />
-		public override IEnumerable<IQueryParamInfo> VisitVariable(VariableContext context)
-		{
-			string variableName = context.GetRuleContexts<IdentifierContext>().Single().GetText();
-
-			_current.DbName = variableName;
-
-			return base.VisitVariable(context);
-		}
-
-		/// <inheritdoc />
-		public override IEnumerable<IQueryParamInfo> VisitTypeName(TypeNameContext context)
-		{
-			IEnumerable<string> typeNameParts = context.GetRuleContexts<IdentifierContext>().Select(p => p.GetText());
-
-			_current.DbType = string.Join(" ", typeNameParts);
-
-			return base.VisitTypeName(context);
-		}
-
-		/// <inheritdoc />
-		public override IEnumerable<IQueryParamInfo> VisitLength(LengthContext context)
-		{
-			var maxValueContext = new Lazy<MaxValueContext>(() => context.GetRuleContext<MaxValueContext>(0));
-			var intContext = new Lazy<IntValueContext>(() => context.GetRuleContext<IntValueContext>(0));
-
-			if (intContext.Value != null)
-			{
-				_current.Length = intContext.Value.GetText();
-			}
-			else if (maxValueContext.Value != null)
-			{
-				_current.Length = maxValueContext.Value.GetText();
-			}
-
-			return base.VisitLength(context);
-		}
-
-		/// <inheritdoc />
-		public override IEnumerable<IQueryParamInfo> VisitValue(ValueContext context)
-		{
-			var stringContext = new Lazy<StringValueContext>(() => context.GetRuleContext<StringValueContext>(0));
-			var intContext = new Lazy<IntValueContext>(() => context.GetRuleContext<IntValueContext>(0));
-			var floatContext = new Lazy<FloatValueContext>(() => context.GetRuleContext<FloatValueContext>(0));
-			
-			if (stringContext.Value != null)
-			{
-				_current.DefaultValue = stringContext.Value.GetText()?.Trim('\'');
-			}
-			else if (intContext.Value != null)
-			{
-				_current.DefaultValue = int.Parse(intContext.Value.GetText());
-			}
-			else if (floatContext.Value != null)
-			{
-				_current.DefaultValue = float.Parse(floatContext.Value.GetText());
-			}
-
-			return base.VisitValue(context);
+			var declarationVisitor = new DeclarationVisitor();
+			IQueryParamInfo info = declarationVisitor.VisitDeclaration(context);
+			return new[] { info };
 		}
 	}
 }
